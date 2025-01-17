791063d1bad3f6123c63dc2c1ebee7311451b030
Albert NIYONSENGA <94793290+Al04ni@users.noreply.github.com>
2025-01-17 11:05:16 +0200

    Added `rand` crate for generating random number and `parse` it for comparing with `guess`

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main(){
     println!("---Guessing the number Game---");
-    println!("Please input of your guess.");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+    
+    loop {
+        println!("Please input of your guess.");
+    
+        let mut guess: String  = String::new();
 
-    let mut guess: String  = String::new();
+        io::stdin()
+        //just to get the user input as mutable variable
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        //println!("the secret number is: {secret_number}.");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        println!("You guessed: {guess}" );
 
-    println!("You guessed: {}", guess);
+        // generating a secret number
+    
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+        }
+    
+    }
+    }
+    
 
 
 }
